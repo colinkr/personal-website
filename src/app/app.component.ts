02eddbbc5f5a82f1c771c9c79bea999a3d88aba1
Colin <83401369+colinkr@users.noreply.github.com>
2023-08-13 01:13:11 +0200

    Version alpha 1.0 - Added picture - Added texts - Added localization for englisch and german - Added imprint - Minor design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personal-website';
+  currentLang: string = 'DE';
+  currentPage: string = 'main';
+
+  changeLang(currentLang: string): void {
+    if(currentLang === 'DE') {
+      this.currentLang = 'EN';
+    } else if(currentLang === 'EN') {
+      this.currentLang = 'DE';
+    }
+  }
+
+  changeSite(link: string): void {
+    this.currentPage = link;
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyA_eKkPqaBX8KiW5xSSKYUjNDpUN2EusQA",
+    authDomain: "personal-website-2bf53.firebaseapp.com",
+    projectId: "personal-website-2bf53",
+    storageBucket: "personal-website-2bf53.appspot.com",
+    messagingSenderId: "741730826305",
+    appId: "1:741730826305:web:1c657a1b18b1d73d75aec0"
+  }
+};
