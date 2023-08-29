d1af1880ee20dcf534ecbe5d3777275a855342b9
Colin <83401369+colinkr@users.noreply.github.com>
2023-08-29 13:25:52 +0200

    Version alpha 1.4 - fixed imprint & privacy bottom margin - added auto scroll when switching pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,5 +20,7 @@ export class AppComponent {
 
   changeSite(link: string): void {
     this.currentPage = link;
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
 }
